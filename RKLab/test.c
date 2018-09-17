88d614a046f811f3233d1d814a40d1fb1a24c826
abdullahzameek <arz268@nyu.edu>
2018-09-17 11:21:02 +0400

    looks like the normaliser actually works properly now

--- RKLab/test.c
+++ RKLab/test.c
@@ -23,60 +23,72 @@ int normalize(char *buf,	/* The character array containing the string to be norm
 					int len			/* the size of the original character array */)
 {
 	int change = 0; //a marker to the change element on the buffer
-	int peek = 0; //a marker to the element behind change
+	int peek = 0; //a marker to the element in front of change, "peeks" at what's ahead
     int spaceCounter = 0; //a counter to keep track of spaces when traversing consecutive spaces.
+    int buflen = len; //this is gonna be used so that the length can be modified in the function
 
-	for(change = 0; change < len; change++)
+
+    for(change = 0; change < len; change++)
 	{   
         //check for uppercase chara and replace with lower
 		if(isupper(buf[change]) > 0){
 			buf[peek++] = tolower(buf[change]);
 		}
 		
+        //if there are no changes to be made, increment peek
         else if((isspace(buf[change])) == 0 && isspace(buf[change+1]) != 0){
 			peek++;
 		}
         
+        //check for consecutive spaces
         else if((isspace(buf[change])) != 0 && isspace(buf[change+1]) != 0){
-			while(isspace(buf[peek]) != 0 && peek < len){
+			//keep moving peek until it hits another letter, or hits the end of the string, and count the spaces along the way
+            while(isspace(buf[peek]) != 0 && peek < len){
                 peek++;
                 spaceCounter++;
             }
+            //adjust the space counter for overshoot, and increment change to keep a single space character behind
             spaceCounter--;
             change++;
 
+            //while peek hasnt hit another letter or hasnt reached the end of the string, swap buf[peek] with buf[change] and move both forward
             while(isspace(buf[peek]) == 0 && peek < len){
                 buf[change] = tolower(buf[peek]);
                 buf[peek] = ' ';
-                peek++; // this is making peek go out of range oops and it tries to overwrite the null terminator fak
+                peek++; 
                 change++;
             }
             
+            //update new length and prevent change from overshooting
             buf[change] = ' ';
-            len = len-spaceCounter;
+            buflen = buflen-spaceCounter;
             spaceCounter = 0;
             change--;
 		}
         
+        //if nothing to be done, move peek
         else
         {
 		    peek++;
 		}
 
+        //fail safe - if either peek or change exceed length, break to prevent stack smashing from occuring
         if(peek >= len || change >= len){
             break;
         }
 	}
+    //update and return len
+    len = buflen;
     return len;
 }
 
 
 int main(int argc, char** argv){
-    int testlen = 39;
-    char abc[39] = "Normaliser fUcK yEAHH UhOHu     G   UWU";
+    char abc[124] = "It   WorKs    fOr shorT   SentenceS   and  it   PROBABLY   DOES   FOR  LONGER       SENTENCES     TOOOO    MAYYYYYBEEeeeEEEE";
+    abc[124] = '\0';
     printf("%s\n", abc);
     printf("Normalizing now\n");
-    int newlen = normalize(abc,39);
+    int newlen = normalize(abc,124); 
     printf("Newlen is %d\n", newlen);
     for(int i = 0; i < newlen; i++){
         printf("%c", abc[i]);
